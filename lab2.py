1e548e5253ab6390007e615d1e7302ad61d6f82e
Marco Centurion <marcocenturion@gmail.com>
2014-05-28 21:18:00 -0300

    commit inicial lab2.py

--- lab2.py
+++ lab2.py
@@ -1,8 +1,25 @@
 import random
+import math
+import hashlib
 default_pseudo_random=random.Random()
 default_crypto_random=random.SystemRandom()
 
 DEFAULT_ITERATION=20
+
+def exp_rapida(n,e,m):
+	print n
+	print e
+	print '-'
+	if e==0:
+		return 1
+	if e==1:
+		return n
+	if e%2==0:
+		return exp_rapida(n**2%m,e/2,m)
+	else:
+		return n*exp_rapida(n**2%m,(e-1)/2,m)%m
+		
+
 
 def sacar_los_2(n):
 	k=0
@@ -15,13 +32,13 @@ def sacar_los_2(n):
 #Devuelve True si un numero es compuesto
 def miller_rabin(n,a):
 	(q,k)=sacar_los_2(n-1)
-	a1=pow(a,q,n)
+	a1=exp_rapida(a,q,n)
 	if a1 == 1:
 		return False
 	for i in range(k):
 		if a1== n-1:
 			return False
-		a1=pow(a1, 2,n)
+		a1=a1**2%n # Hacer exp_rapida aca solo me meteria en una recursion innecesaria
 	return True
 
 
@@ -54,4 +71,46 @@ def hallar_primo(size=128, rnd=default_crypto_random, k=DEFAULT_ITERATION):
 			return n
 	return -1
     
-print hallar_primo()
+def os2ip(x):
+	return int(x.replace(' ',''),16)
+    
+def i2osp(x, x_len):
+	y=(hex(x)[2:])[::-1]
+	# Tomo la representacion hexa, le quito el '0x' y la invierto
+	y=('{:0<'+str(2*x_len)+'}').format(y)
+	# Padeo con ceros
+	y=(' '.join(y[i:i+2] for i in xrange(0,len(y),2)))[::-1]
+	# Separo en pares e invierto de nuevo
+	
+	return y
+	
+def rsaep(m, n):
+	#m es la clave y n el mensaje
+	# modulo    = m[0]
+	# exponente = m[1]
+	return 0
+	
+def mgf1(mgf_seed, mask_len, hash_class=hashlib.sha1):
+	T=''
+	h=hash_class()
+	h_len=h.digest_size
+	tope=4294967296*h_len
+	if mask_len<=tope:
+		for i in range(int(math.ceil(mask_len/h_len))):
+			h.update(mgf_seed+i2osp(i,4))
+			T=T+h.hexdigest()
+		return T
+	
+	
+print mgf1('asfqewwww',2**6)
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
