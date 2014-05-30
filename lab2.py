bb929e9135eb7e521aa0f48f75997f031d496e16
Marco Centurion <marcocenturion@gmail.com>
2014-05-30 02:04:14 -0300

    encriptacion pronta, falta desencriptacion y testeo del tema de las mascaras

--- lab2.py
+++ lab2.py
@@ -1,26 +1,49 @@
 import random
 import math
 import hashlib
+import sys
 
 # Constantes
 default_pseudo_random=random.Random()
 default_crypto_random=random.SystemRandom()
 DEFAULT_ITERATION=20
 
+#MCD
+def mcd(a,b):
+    r=1
+    while r>0:
+        r=a%b
+        (a,b)=(b,r)
+    return a        
 
-# Exponenciacion rápida con módulo
+
+#XMCD
+def xmcd(a,b):
+    # Seteo los valores iniciales para
+    # el algoritmo
+    (ui,uj,vi,vj)=(1,0,0,1)
+    r=1
+    while(r>0):
+        # asigno los valores a las variables
+        (q,r)=(a//b,a%b)
+        (u,v)=(ui-uj*q,vi-vj*q)
+        # Preparo la siguiente iteracion
+        (a,b,ui,uj,vi,vj)=(b,r,uj,u,vj,v)
+    # Devuelvo los valores de la iteracion previa
+    return (a, ui, vi)
+
+
+# Exponenciacion rapida con modulo
 def exp_rapida(n,e,m):
-	print n
-	print e
-	print '-'
-	if e==0:
-		return 1
-	if e==1:
-		return n
-	if e%2==0:
-		return exp_rapida(n**2%m,e/2,m)
-	else:
-		return n*exp_rapida(n**2%m,(e-1)/2,m)%m
+	resultado=1
+	while e!=0:
+		if e%2!=0:
+			resultado=resultado*n%m
+			e-=1
+		n=n**2%m
+		e=e/2
+	return resultado
+			
 		
 
 # Funcion auxiliar para Miller-Rabin, saca los factores de 2 y devuelve la 
@@ -97,12 +120,52 @@ def rsaep(m, n):
 	# m es la clave y n el mensaje
 	#  modulo    = m[0]
 	#  exponente = m[1]
-	return 0
+	if (n<0) or (n>m[0]-1):
+		sys.exit('message representative out of range')
+	return exp_rapida(n,m[1],m[0])
 	
 # Primitiva de desencriptacion de RSA
 def rsadp(c,m):
+	# c es la clave de la especificacion, hay dos casos
+	# 	1) c=(n,d), donde n es el modulo y d el exponente
+	#	2) c=((p,q,dP,dQ,qInv),[(ri,di,ti)])
+	#
+	#
 	
-	return 0
+	if isinstance(c[1],list):
+		p,q,dp,dq,qinv=c[0]
+		tuplas=c[1]
+		m1=exp_rapida(m,dp,p)
+		m2=exp_rapida(m,dq,q)
+		emes=list()
+		for i in range(len(tuplas)):
+			emes.append(exp_rapida(m,tuplas[i][1],tuplas[i][0]))
+		h=(m1-m2)*qinv%p
+		m=m2+q*h
+		
+		print len(tuplas)
+		if len(tuplas)>0:
+			erres=[p,q]
+			des=[dp,dq]
+			tes=[0,qinv]
+			for i in range(len(tuplas)):
+				erres.append(tuplas[i][0])
+				des.append(tuplas[i][1])
+				tes.append(tuplas[i][2])	
+			R=erres[0]
+			print len(erres)
+			print len(des)
+			print len(tes)
+			for i in range(2,len(erres)):
+				print 'iteracion '
+				print i-2
+				R=R*erres[i-1]
+				mi=exp_rapida(m,des[i],erres[i])
+				h=(mi-m)*tes[i]%erres[i]
+				m=m+R*h
+		return m			
+	m=exp_rapida(m,c[1],c[0])
+	return m
 
 
 # Devuelve una mascara como especificado en el documento
@@ -117,4 +180,25 @@ def mgf1(mgf_seed, mask_len, hash_class=hashlib.sha1):
 			T=T+h.hexdigest()
 		return T
 	
+e=2**16-1
+p=1299743
+q=1299827
+
+d=xmcd(e,(p-1)*(q-1))[1]
+
+mm = (p-1)*(q-1)
+
+r1,r2,r3,r4=(1299743, 1299827, 1299833, 1299887)
+t3, t4 = 392357,538006 
+d3, d4 = 67575,878571 
+dP, dQ = 848189,303739 
+qInv = 355905 
+n = r1*r2*r3*r4
+ 
+ 
+print rsadp(((r1,r2,dP,dQ,qInv), [(r3,d3,t3),(r4,d4,t4)]),e) 
+print  rsaep((n,e),1008117944153308176100846) 
+
+
+
 
